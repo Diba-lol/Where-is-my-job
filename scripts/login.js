2d336553d1672a0184248d4c1a357e9efaf0f541
n4zr-sketcher <nazeer.game07@gmail.com>
2026-09-17 21:46:53 +0530

    added default id & pass to login

--- scripts/login.js
+++ scripts/login.js
@@ -33,3 +33,24 @@ loginBtn.addEventListener("click", function () {
 
   }
 });
+
+var emailInput = document.getElementById("login_mail");
+var emailPass = document.getElementById("login_pass");
+var loginBtn = document.getElementById("login_btn");
+var loginError = document.querySelector(".loginBox_error");
+
+loginBtn.addEventListener("click", function () {
+  var enteredEmail = emailInput.value;
+  var enteredPassword = emailPass.value;
+
+  if (enteredEmail === "user@gmail.com" && enteredPassword === "123456") {
+
+    loginError.style.display = "none";
+    window.location.href = "./pages/home.html";
+
+  } else {
+
+    loginError.style.display = "block";
+
+  }
+});
